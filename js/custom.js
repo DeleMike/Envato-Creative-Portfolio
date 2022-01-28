2a87a24710fdadffd2a94a27ff495bdf1c4b5e5f
DeleMike <akindelemichael65@gmail.com>
2022-01-28 15:08:15 +0100

    create countdown timer

--- js/custom.js
+++ js/custom.js
@@ -12,3 +12,28 @@ for (var i = 0; i < btns.length; i++) {
         this.className += " active";
     });
 }
+
+const countdown = () => {
+    const countDate = new Date('May 17, 2022 00:00:00').getTime()
+    const now = new Date()
+    const gap = countDate - now
+
+    const second = 1000
+    const minute = second * 60
+    const hour = minute * 60
+    const day = hour * 24
+
+    const textDay = Math.floor(gap / day)
+    const textHour = Math.floor((gap % day) / hour)
+    const textMinute = Math.floor((gap % hour) / minute)
+    const textSecond = Math.floor((gap % minute) / second)
+
+    document.querySelector('.day').innerHTML = textDay
+    document.querySelector('.hour').innerHTML = textHour
+    document.querySelector('.minute').innerHTML = textMinute
+    document.querySelector('.second').innerHTML = textSecond
+
+}
+
+setInterval(countdown, 1000)
+
