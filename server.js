e48c59c9f3679eb8fe6a4dadedb3cbce6a941251
DeleMike <akindelemichael65@gmail.com>
2022-02-22 14:51:20 +0100

    add docs and fix transport medium

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//import necessary packages
 require('dotenv').config();
 
 const express = require('express')
@@ -7,36 +8,60 @@ const app = express()
 
 const port = process.env.PORT || 3000;
 
-// middleware
+/*
+middleware
+Add middle ware for server to identify public files
+*/
 app.use(express.static('./public'));
 app.use(express.json());
 
+//home page
 app.route("/").get(function (req, res) {
   res.sendFile(process.cwd() + "/public/index.html");
 });
 
 app.post("/send", (req, res) => {
-  //1.
   let form = new multiparty.Form();
   let data = {};
+  //collect all data from input fields on contact page
   form.parse(req, function (err, fields) {
     console.log(fields);
     Object.keys(fields).forEach(function (property) {
       data[property] = fields[property].toString();
+    })
+
+    //create a transport medium.
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN
+      }
     });
 
-    console.log(data);
-
+    // console.log(data);
+    //prepare mailing object
     const mailOptions = {
       from: `"${data.firstname}" ${data.email}`,
       to: process.env.MAIL_USERNAME,
       subject: data.subject,
-      text:  `${data.requestDetail}`,
-     // html: "<h2> This is a test Email</h2>"
+      text: `${data.requestDetail}`,
+      // html: "<h2> This is a test Email</h2>"
     };
 
-    console.log('Mail Options Sender: ', `${mailOptions.from}`);
+    // console.log('Mail Options Sender: ', `${mailOptions.from}`);
+    
+    /*
+    send email to sender
 
+    This transporter works for only GMAIL account. A more robust way should be put in place for
+    it to work properly and ensure scalability.
+    Other mail services sent to the receipent might be blocked or sent to the 'spam' folder.
+    */
     transporter.sendMail(mailOptions, (err, data) => {
       if (err) {
         console.log(err);
@@ -46,21 +71,6 @@ app.post("/send", (req, res) => {
     });
   });
 });
-
-
-let transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    type: 'OAuth2',
-    user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN
-  }
-});
-
-
 
 
 app.listen(port, () => {
